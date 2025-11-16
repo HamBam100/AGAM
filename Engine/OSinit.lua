e77fba17fc8d115cba39a16c62a8d6c039309417
HamBam100 <hambam100@gmail.com>
2025-11-16 16:51:41 +1000

    Auto push from Push.sh

--- Engine/OSinit.lua
+++ Engine/OSinit.lua
@@ -1,9 +1,19 @@
-local OS = love.system.getOS()
 
+
+
+local OS = love.system.getOS()
+local dir = love.filesystem.getSourceBaseDirectory()
 
 if OS == "Windows" then
-    package.cpath = package.cpath .. ";Steam/Windows/?.dll"
+    package.cpath = package.cpath .. ';' .. dir .. '/Steam/Windows/?.dll'
+elseif OS == "Linux" then
+    dir = dir .."/AGAM"
+    package.loadlib(dir .. "/libsteam_api.so", "*")
+    
+    package.cpath = package.cpath .. ';' .. dir .. '/Steam/Linux/?.so'
+
 end
 
 
 Steam = require "luasteam"
+
